2520ca9a8ca8892af26f88c8804a0a9eacacbc1a
Vincentdm31 <vincent.dimarco1@gmail.com>
2021-04-30 10:32:27 +0200

    migrations + charts install

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+
+            DB::table('users')->insert(
+                [
+                    'name' => $faker->name(),
+                    'email' => $faker->email(),
+                    'password' => Hash::make('azsqazsq'),
+                    'actualMoney' => rand(0, 500000),
+                    'role' => 0,
+                    'buildings' => json_encode([
+                        'Tipi' => rand(0,5),
+                        'Cabane' => rand(0,5),
+                        'Maison' => rand(0,5),
+                        'Villa' => rand(0,5),
+                        'Temple' => rand(0,5),
+                    ]),
+                    'created_at' => Carbon::today()->subDays(rand(0, 30)),
+                    'updated_at' => Carbon::now(),
+                ]
+            );
+        }
+
+        DB::table('users')->insert(
+            [
+                'name' => 'legrosdep',
+                'email' => 'az@az.az',
+                'password' => Hash::make('azsqazsq'),
+                'actualMoney' => 1000000,
+                'role' => 1,
+                'buildings' => json_encode([
+                    'Tipi' => 10,
+                    'Cabane' => 10,
+                    'Maison' => 10,
+                    'Villa' => 10,
+                    'Temple' => 10
+                ]),
+                'created_at' => Carbon::today()->subDays(rand(0, 30)),
+            ]
+        );
     }
 }
