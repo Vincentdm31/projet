e9c2348bd2f600ab857e9b3bde7f2b35f13f7609
Vincentdm31 <vincent.dimarco1@gmail.com>
2021-04-30 12:52:16 +0200

    test

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -118,7 +118,7 @@ class AuthController extends Controller
             ->json([
                 'access_token' => $token,
                 'token_type' => 'bearer',
-                'expires_in' => auth()->factory()->getTTL() * 60,
+                'expires_in' => auth('api')->factory()->getTTL() * 60,
                 'user' => auth()->user()
             ]);
     }
