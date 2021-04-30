e6dc1eed9ed03a57f30ccb412ec3b24565869254
Vincentdm31 <vincent.dimarco1@gmail.com>
2021-04-30 11:32:50 +0200

    validator

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Validation\Validator;
+use Validator;
 
 class AuthController extends Controller
 {
